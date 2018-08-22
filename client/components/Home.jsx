769bd8dadcb6263eb4a9923a5de82726b48cac89
adam-kuhn <adam.kuhn20@gmail.com>
2018-08-22 09:35:49 +0300

    nav in its own component and nav to home

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,22 +1,9 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
 
 class Home extends React.Component {
   render () {
     return (
-      <div>
-        <h2>HOME COMPONENT</h2>
-        <Link to='/about-me'>
-          <button>
-            About Me
-          </button>
-        </Link>
-        <Link to='/projects'>
-          <button>
-            Projects
-          </button>
-        </Link>
-      </div>
+      <h2>HOME COMPONENT</h2>
     )
   }
 }
