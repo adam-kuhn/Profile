ae48a73caef89f6b26c60fbae20bf8f152b89d8a
adam-kuhn <adam.kuhn20@gmail.com>
2018-09-18 16:13:10 +0300

    able to play youtube video from API call

--- client/components/Nasa/Nasa.jsx
+++ client/components/Nasa/Nasa.jsx
@@ -12,11 +12,13 @@ class Nasa extends React.Component {
   componentDidMount () {
     request.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
       .then(result => {
-        console.log(result.body)
         this.setState({
           nasa: result.body,
           infoRetrieved: true
         })
+      })
+      .catch(err => {
+        console.error(err)
       })
   }
   render () {
@@ -25,14 +27,11 @@ class Nasa extends React.Component {
       return (
         <div>
           <h3>{title}</h3>
-          {/* {media_type === 'video'
-            ? <video>
-              <source src={url} type="video/mp4"/>
-            </video>
-            : <img src={url} />} */}
+          {media_type === 'video'
+            ? <iframe src={url}/>
+            : <img src={url} />}
           <p>Date: {date}</p>
           <p>Description: {explanation}</p>
-          <img src="https://www.google.fi/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj85fbqx8TdAhXOp4sKHSNVAGEQjRx6BAgBEAU&url=https%3A%2F%2Fwww.smithsonianmag.com%2Fscience-nature%2Fgreening-future-outer-space-180969212%2F&psig=AOvVaw33n6_dJ7bfzxWqB6kH5369&ust=1537360281778881" />
         </div>
       )
     } else {
