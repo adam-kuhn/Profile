e24af786df29c646676787e3cd04d78c51a5b806
adam-kuhn <adam.kuhn20@gmail.com>
2018-09-18 15:39:27 +0300

    accessing NASA apod API but unable to load image or video because of CORB

--- client/components/Nasa/Nasa.jsx
+++ client/components/Nasa/Nasa.jsx
@@ -1,15 +1,43 @@
 import React from 'react'
+import request from 'superagent'
 
 class Nasa extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      nasa: {},
+      infoRetrieved: false
+    }
+  }
   componentDidMount () {
-    // some api call
+    request.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(result => {
+        console.log(result.body)
+        this.setState({
+          nasa: result.body,
+          infoRetrieved: true
+        })
+      })
   }
   render () {
-    return (
-      <div>
-        <h3>Nasa Pic</h3>
-      </div>
-    )
+    const {date, explanation, title, media_type, url} = this.state.nasa
+    if (this.state.infoRetrieved) {
+      return (
+        <div>
+          <h3>{title}</h3>
+          {/* {media_type === 'video'
+            ? <video>
+              <source src={url} type="video/mp4"/>
+            </video>
+            : <img src={url} />} */}
+          <p>Date: {date}</p>
+          <p>Description: {explanation}</p>
+          <img src="https://www.google.fi/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj85fbqx8TdAhXOp4sKHSNVAGEQjRx6BAgBEAU&url=https%3A%2F%2Fwww.smithsonianmag.com%2Fscience-nature%2Fgreening-future-outer-space-180969212%2F&psig=AOvVaw33n6_dJ7bfzxWqB6kH5369&ust=1537360281778881" />
+        </div>
+      )
+    } else {
+      return (<p>Fetching cool things</p>)
+    }
   }
 }
 
