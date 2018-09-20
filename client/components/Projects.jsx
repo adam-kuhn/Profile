dcf74cbc294766a8779fc7536be3e176cb5dc6dd
adam-kuhn <adam.kuhn20@gmail.com>
2018-09-20 15:16:20 +0300

    set up basic component layout of projects

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <h2>Look at my sweet projects</h2>
+    <div>
+      <h2>Look at my sweet projects</h2>
+      <div>
+        <h3>Deltage</h3>
+        <img src="/images/deltage_scr-shot.png" />
+        <p>Deltage is this...</p>
+        <a href="http://deltage.nz/">Checkout it out!</a>
+        <a href="https://github.com/TylerGriffin99/Deltage">Checkout the code!</a>
+      </div>
+    </div>
   )
 }
 
