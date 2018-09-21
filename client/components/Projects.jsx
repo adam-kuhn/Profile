521de2dbf21b230bec56dc0eb4488cc991bccc27
adam-kuhn <adam.kuhn20@gmail.com>
2018-09-21 08:05:23 +0300

    move deltage code to own file

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 
+import Deltage from './Projects/Deltage/Deltage'
+
 const Projects = () => {
   return (
     <div>
       <h2>Look at my sweet projects</h2>
-      <div>
-        <h3>Deltage</h3>
-        <img src="/images/deltage_scr-shot.png" />
-        <p>Deltage is this...</p>
-        <a href="http://deltage.nz/">Checkout it out!</a>
-        <a href="https://github.com/TylerGriffin99/Deltage">Checkout the code!</a>
-      </div>
+      <Deltage />
     </div>
   )
 }
---
+++ client/components/Projects/Deltage/Deltage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Deltage = () => {
+  return (
+    <div>
+      <h3>Deltage</h3>
+      <img src="/images/deltage_scr-shot.png" />
+      <p>Deltage is this...</p>
+      <a href="http://deltage.nz/">Checkout it out!</a>
+      <a href="https://github.com/TylerGriffin99/Deltage">Checkout the code!</a>
+    </div>
+  )
+}
+
+export default Deltage
