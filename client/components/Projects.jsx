da6822594e9783b07948efa2bede6a81fffa91a8
adam-kuhn <adam.kuhn20@gmail.com>
2018-09-22 13:17:05 +0300

    move projects into a standardized compomonent

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 
-import Deltage from './Projects/Deltage/Deltage'
-import RaceFuel from './Projects/RaceFuel/RaceFuel'
+import StandardProject from './Projects/StandardProj/StandardProj'
 
 const Projects = () => {
   return (
     <div>
       <h2>Look at my sweet projects</h2>
-      <Deltage />
-      <RaceFuel />
+      <StandardProject
+        title='Deltage'
+        image='/images/deltage_scr-shot.png'
+        desc='Deltage does this...'
+        live='http://deltage.nz/'
+        code='https://github.com/TylerGriffin99/Deltage'
+      />
+      <StandardProject
+        title='Race Fuel'
+        image='/images/racefuel_scr-shot.png'
+        desc='Race Fuel allows racers to track nutrtion through long endurance events...'
+        live={false}
+        code='https://github.com/adam-kuhn/race-fuel'
+      />
     </div>
   )
 }
