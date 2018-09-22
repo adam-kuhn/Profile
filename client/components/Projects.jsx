658da22a845949be6013559b3064bb107fd0c124
adam-kuhn <adam.kuhn20@gmail.com>
2018-09-22 12:56:04 +0300

    add RaceFuel to projects

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import Deltage from './Projects/Deltage/Deltage'
+import RaceFuel from './Projects/RaceFuel/RaceFuel'
 
 const Projects = () => {
   return (
     <div>
       <h2>Look at my sweet projects</h2>
       <Deltage />
+      <RaceFuel />
     </div>
   )
 }
