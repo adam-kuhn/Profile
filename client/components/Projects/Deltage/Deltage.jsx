64af845531f2e153ab0ff152dfc248bc7f0ed0b3
adam-kuhn <adam.kuhn20@gmail.com>
2018-09-22 12:43:46 +0300

    create basic styling for projects

--- client/components/Projects/Deltage/Deltage.jsx
+++ client/components/Projects/Deltage/Deltage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import '../projects.css'
+
 const Deltage = () => {
   return (
-    <div>
+    <div className='proj'>
       <h3>Deltage</h3>
-      <img src="/images/deltage_scr-shot.png" />
+      <img className='proj-img' src="/images/deltage_scr-shot.png" />
       <p>Deltage is this...</p>
       <a href="http://deltage.nz/">Checkout it out!</a>
       <a href="https://github.com/TylerGriffin99/Deltage">Checkout the code!</a>
