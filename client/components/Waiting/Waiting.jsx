b0aa48e263848d2ce5bfc6f54d868940d027300c
adam-kuhn <adam.kuhn20@gmail.com>
2018-09-25 11:52:05 +0300

    add center styling to wait icon

--- client/components/Waiting/Waiting.jsx
+++ client/components/Waiting/Waiting.jsx
@@ -4,7 +4,7 @@ import './waiting.css'
 
 const Waiting = () => {
   return (
-    <div>
+    <div className='container-vert'>
       <img className='waiting-icon' src='./images/waiting.png' />
       <p>Please wait while we access cool stuff from NASA!</p>
     </div>
