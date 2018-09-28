71e150a8ad64ee29cdf32deb16d6dca81f58fa63
adam-kuhn <adam.kuhn20@gmail.com>
2018-09-28 09:06:42 +0300

    add blurb about my abilities and minor linting

--- client/components/About/SkillSet/SkillSet.jsx
+++ client/components/About/SkillSet/SkillSet.jsx
@@ -21,9 +21,24 @@ class SkillSet extends React.Component {
   render () {
     return (
       <div>
-        <p>My Skills are being held in a database. Press the button to access my skills</p>
-        <button type='button' onClick={this.handleTech}>Fetch Tech Skills</button>
-        <button type='button' onClick={this.handleSoft}>Fetch Soft Skills</button>
+        <p>
+          My Skills are being held in a database.
+          Press the button to access my skills
+        </p>
+        <p>
+          Having only been coding for just about 1
+          year I am not naive enough to say that I
+          have mastered any of the below technologies.
+          But I am however extremly proficient with
+          them and I am always learning and growing
+          and I know that one day I will be a <q>Master</q>.
+        </p>
+        <button type='button' onClick={this.handleTech}>
+          Fetch Tech Skills
+        </button>
+        <button type='button' onClick={this.handleSoft}>
+          Fetch Soft Skills
+        </button>
         <ul>
           {this.props.skills.map(skill => {
             return (
