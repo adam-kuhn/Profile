965df2f379ca8a9b05b7d0511bb5755b83c45ba3
adam-kuhn <adam.kuhn20@gmail.com>
2018-10-11 11:17:07 +0300

    rmv "hi" -> was only there to test something

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,11 +6,9 @@ import Home from './Home/Home'
 import About from './About/About'
 import Projects from './Projects/Projects'
 
-const name = 'hi'
 const App = () => {
   return (
     <div>
-      <p>{name}</p>
       <Nav />
       <Route exact path='/' component={Home} />
       <Route path='/about-me' component={About} />
